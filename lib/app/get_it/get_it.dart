838a18c6ba4793e56f08a217da89e065a9892041
mehmetnf <mehmetnf99@gmail.com>
2023-12-06 01:07:18 +0300

    Get-it file created

---
+++ lib/app/get_it/get_it.dart
@@ -0,0 +1,38 @@
+import 'package:get_it/get_it.dart';
+import 'package:news_app/app/data/repository/home_repository.dart';
+import 'package:news_app/app/data/repository/i_home_repository.dart';
+import 'package:news_app/app/data/service/home_service.dart';
+import 'package:news_app/app/data/service/i_home_service.dart';
+import 'package:news_app/app/screens/home/viewmodel/home_viewmodel.dart';
+import 'package:news_app/app/screens/main/viewmodel/main_viewmodel.dart';
+import 'package:news_app/app/screens/splash/viewmodel/splash_viewmodel.dart';
+
+final getIt = GetIt.instance;
+
+void setupGetIT() {
+  setupViewModel();
+  setupRepository();
+  setupServices();
+}
+
+void setupViewModel() {
+  getIt
+    ..registerLazySingleton<SplashViewModel>(
+      () => SplashViewModel(),
+    )
+    ..registerLazySingleton<MainViewModel>(
+      () => MainViewModel(),
+    )
+    ..registerLazySingleton<HomeViewModel>(
+      () => HomeViewModel(iHomeRepository: getIt()),
+    );
+}
+
+void setupRepository() {
+  getIt.registerLazySingleton<IHomeRepository>(
+      () => HomeRepository(iHomeService: getIt()));
+}
+
+void setupServices() {
+  getIt.registerLazySingleton<IHomeService>(HomeService.new);
+}
