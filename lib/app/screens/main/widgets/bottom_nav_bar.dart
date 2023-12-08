abe3ce092df394c1260836a885cc7fa050e99614
mehmetnf <mehmetnf99@gmail.com>
2023-12-08 11:45:35 +0300

    Bottom nav bar update

--- lib/app/screens/main/widgets/bottom_nav_bar.dart
+++ lib/app/screens/main/widgets/bottom_nav_bar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:news_app/app/constants/app_strings.dart';
 import 'package:news_app/app/screens/main/viewmodel/main_viewmodel.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 
@@ -17,13 +18,13 @@ class BottomNavBar extends StatelessWidget {
       builder: (context) {
         return PersistentTabView(
           navBarHeight: 70,
-          padding: const NavBarPadding.all(3),
+          padding: const NavBarPadding.symmetric(horizontal: 25),
           context,
           controller: _viewModel.controller,
           screens: _viewModel.buildScreens,
           items: _viewModel.navBarItems,
           confineInSafeArea: true,
-          backgroundColor: const Color.fromRGBO(242, 242, 242, 1),
+          backgroundColor: cLightPrimaryColor,
           hideNavigationBarWhenKeyboardShows: true,
           popAllScreensOnTapOfSelectedTab: true,
           popActionScreens: PopActionScreensType.all,
